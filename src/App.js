66c9064c6d6001d42b7c722370fa4e13078a08d3
Brendan <brendan@Brendans-Air.turbospeed.home>
2020-08-22 12:28:13 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const getQuote = () => {
+    fetch('https://api.kanye.rest')
+    .then(response => response.json())
+    .then(data => document.getElementById('sex').innerHTML = data.quote)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+    <h1>Get your free kanye quote below</h1>
+
+    <h3 id="sex">  </h3>
+
+    <button onClick={getQuote} className="myButton"> get quote </button>
+
     </div>
   );
 }
 
-export default App;
+export default App
+
